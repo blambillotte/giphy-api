639eb54a06e2c6de8175fe7c7d0cb98246407d2d
Ben Lambillotte <ben.lambillotte@gmail.com>
2017-09-20 22:47:39 -0700

    Added user input API Key

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,165 +1,218 @@
 
-var url = 'https://api.giphy.com/v1/gifs/search';
-var noConnectWarn = $('#no-connection');
+$(document).ready(function() {
 
-function callAPI(searchString) {
-  url += '?' + $.param({
-    'api_key': API_KEY,
-    'q': searchString,
-  });
-  $.ajax({
-    url: url,
-    method: 'GET',
-  }).done(function(response) {
+  var url = 'https://api.giphy.com/v1/gifs/search';
+  var API_KEY;
 
-    handleResults(response.data);
+  // Cache DOM
+  var $gifDivTarget = $("#gifs-appear-here");
+  var $btnInput = $('#btn-input');
+  var $btnDiv = $('#buttons');
+  var $noConnectWarn = $('#no-connection');
+  var $addNewBtn = $('#add-btn');
+  var $removeWarningBtn = $('#close-error');
+  var $nullStateDiv = $('#null-state');
+  var $APIKeyInput = $('#API_KEY');
+  var $APIKeySaveBtn = $('#API_Add_Btn');
 
-  }).fail(function(err) {
-    noConnectWarn.removeClass('invisible');
-    throw err;
-  });
-};
+  /*
+  ************ Main App Functions *******************
+  */
 
-function handleResults(responseData) {
-  // console.log(responseData);
-  var results = responseData;
-
-  //Clear DOM for new results
-  clearResults();
-  removeNullState();
-
-  //Loop through image results
-  for (var i = 0; i < results.length; i++) {
-    var gifDiv = $("<div class='gif-div'>");
-    var rating = results[i].rating;
-    var p = $("<p>").text("Rating: " + rating);
-    var gifImg = $("<img>");
-
-    gifImg.attr({
-      src: results[i].images.fixed_height_still.url,
-      'data-animate': results[i].images.fixed_height.url,
-      'data-still': results[i].images.fixed_height_still.url,
-      'data-state': 'still',
-      class: 'gif inactive fade-in'
+  function callAPI(searchString) {
+    var searchUrl = url + '?' + $.param({
+      'api_key': API_KEY,
+      'q': searchString,
+      'limit': 10
     });
+    console.log(searchUrl);
+    $.ajax({
+      url: searchUrl,
+      method: 'GET',
+    }).done(function(response) {
 
-    //Call Write Dom Function
-    writeDom(p, gifImg, gifDiv)
+      handleResults(response.data);
 
-  }
-  clickListen();
-
-};
-
-function clearResults() {
-  $("#gifs-appear-here").empty();
-};
-
-function writeDom(p, gifImg, gifDiv) {
-  gifDiv.prepend(p);
-  gifDiv.prepend(gifImg);
-  $("#gifs-appear-here").prepend(gifDiv);
-};
-
-/*
-************ Create Buttons *******************
-*/
-
-
-var buttons = ['Dog', 'Cat', 'Frog', 'Tiger', 'Fish', 'Cow'];
-var btnInput = $('#btn-input');
-var btnDiv = $('#buttons');
-// Function for displaying movie data
-function renderButtons() {
-  //Clear existing values
-  btnDiv.empty();
-
-  for (var i = 0; i < buttons.length; i++) {
-    var btn = $('<input>');
-    btn.attr({
-      type: 'button',
-      value: buttons[i],
-      title: buttons[i],
-      class: 'btn btn-success'
-    })
-    btnDiv.append(btn);
-    btn.on('click', function(event) {
-      callAPI(this.value);
+    }).fail(function(err) {
+      $noConnectWarn.removeClass('invisible');
+      throw err;
     });
   }
-  //Clear Input each time
-  btnInput.val('');
 
-}
+  function handleResults(responseData) {
+    // console.log(responseData);
+    var results = responseData;
 
-// This function adds new buttons
-$('#add-btn').on('click', function(event) {
+    //Clear DOM for new results
+    clearResults();
+    removeNullState();
 
-  event.preventDefault();
+    //Loop through image results then write to DOM
+    for (var i = 0; i < results.length; i++) {
+      var gifDiv = $("<div class='gif-div'>");
+      var rating = results[i].rating;
+      var p = $("<p>").text("Rating: " + rating);
+      var gifImg = $("<img>");
 
-  //Check if input is null
-  if (btnInput[0].value != '') {
-    buttons.push(btnInput[0].value);
-    //Rerender buttons
-    renderButtons();
+      gifImg.attr({
+        src: results[i].images.fixed_height_still.url,
+        'data-animate': results[i].images.fixed_height.url,
+        'data-still': results[i].images.fixed_height_still.url,
+        'data-state': 'still',
+        class: 'gif inactive fade-in'
+      });
+
+      //Call Write Dom Function
+      writeDom(p, gifImg, gifDiv);
+
+    }
+
+    clickListen();
+
   }
 
-});
+  function clearResults() {
+    $gifDivTarget.empty();
+  }
 
-renderButtons();
+  function writeDom(p, gifImg, gifDiv) {
+    gifDiv.prepend(p);
+    gifDiv.prepend(gifImg);
+    $gifDivTarget.prepend(gifDiv);
+  }
 
-/*
-************ Handle Animate/Sill Swap on Click *******************
-*/
+  /*
+  ************ Create Buttons *******************
+  */
 
-function clickListen() {
 
-  $('.gif').on('click', function() {
+  var BUTTONS = ['Dog', 'Cat', 'Frog', 'Tiger', 'Fish', 'Cow'];
+  // Function for displaying movie data
+  function renderButtons() {
+    //Clear existing values
+    $btnDiv.empty();
 
-    var state = this.dataset.state;
-    console.log(state);
-    console.log(this);
-
-    var animateGif = this.dataset.animate;
-    if (state === 'still') {
-      $(this).attr({
-        src: animateGif,
-        'data-state': 'animate'
+    for (var i = 0; i < BUTTONS.length; i++) {
+      var btn = $('<input>');
+      btn.attr({
+        type: 'button',
+        value: BUTTONS[i],
+        title: BUTTONS[i],
+        class: 'btn btn-success'
       });
-      $(this).removeClass('inactive');
+      $btnDiv.append(btn);
+      btn.on('click', function(event) {
+        callAPI(this.value);
+      });
+    }
+    //Clear Input each time
+    $btnInput.val('');
+  }
+
+  renderButtons();
+
+  // This function adds new button based on user input.
+  $addNewBtn.on('click', function(event) {
+
+    event.preventDefault();
+
+    //Check if input is null
+    if ($btnInput[0].value != '') {
+      BUTTONS.push($btnInput[0].value);
+      //Rerender BUTTONS
+      renderButtons();
     }
 
-    var stillGif = this.dataset.still;
-    if (state === 'animate') {
-      $(this).attr({
-        src: stillGif,
-        'data-state': 'still'
-      });
-      $(this).addClass('inactive');
-    }
   });
-}
+
+  /*
+  ************ Handle Animate/Sill Swap on Click *******************
+  *     Since gifs are not present in DOM on page load
+        I added a function to listen after the gifs are loaded.
+  */
+
+  function clickListen() {
+
+    $('.gif').on('click', function() {
+
+      var state = this.dataset.state;
+      console.log(state);
+      console.log(this);
+
+      var animateGif = this.dataset.animate;
+      var stillGif = this.dataset.still;
+
+      if (state === 'still') {
+        $(this).attr({
+          src: animateGif,
+          'data-state': 'animate'
+        });
+        $(this).removeClass('inactive');
+
+      } else if (state === 'animate') {
+        $(this).attr({
+          src: stillGif,
+          'data-state': 'still'
+        });
+        $(this).addClass('inactive');
+      }
+    });
+  }
 
 
-/************ Dismiss Connection Error *******************
-*     This could be reused for more types dismissable
-*     items since code looks to parent's div
-*/
+  /************ Dismiss Connection Error *******************
+  *     This could be reused for more dismissable
+  *     items since code looks to parent's div.
+  */
 
-var removeWarningBtn = $('#close-error');
 
-removeWarningBtn.on('click', function() {
-  $(this).parent().addClass('invisible');
+  $removeWarningBtn.on('click', removeWarning);
+
+  function removeWarning() {
+    $(this).parent().addClass('invisible');
+  }
+
+
+  /************ Remove Null State ******************
+  *     This gets called by the sucesful loading
+        of the API response.
+  */
+
+  function removeNullState() {
+    $nullStateDiv.addClass('invisible');
+  }
+
+
+  // $APIKeyInput
+  // $APIKeySaveBtn
+
+  //Calling the API with empty params to check validity of API Key
+  // TODO: figure out how to not duplicate the key
+  function testAPIKey(testKey) {
+    testURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + testKey;
+    // console.log(url);
+    $.ajax({
+      url: testURL,
+      method: 'GET',
+    }).done(function(response) {
+      console.log('Key is Valid');
+      //Set global API Key Variable
+      API_KEY = testKey;
+    }).fail(function(err) {
+      $noConnectWarn.removeClass('invisible');
+      throw err;
+    });
+  }
+
+  function setAPIKey() {
+    console.log($APIKeyInput.val());
+    testAPIKey($APIKeyInput.val().trim());
+  }
+
+  $APIKeySaveBtn.on('click', function(){
+    event.preventDefault();
+    setAPIKey();
+  });
+
+
 });
-
-
-/************ Remove Null State ******************
-*     This gets called by the sucesful loading
-      of the API response.
-*/
-
-var nullStateDiv = $('#null-state');
-
-function removeNullState() {
-  nullStateDiv.addClass('invisible');
-}
